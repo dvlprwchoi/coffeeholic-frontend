ec578c5b13500215320527a9065aebe48d493c10
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-08-09 22:38:02 -0500

    Change home page image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Container, Image } from 'react-bootstrap';
-import coffeeBeanImg from '../../img/coffee-beans.jpg';
+import coffeeBeanMugImg from '../../img/coffee-beans-mug.jpg';
 
 const Home = () => {
   return (
     <Container className="p-5 border rounded-3 bg-light">
       <h1>Coffee Holic</h1>
-      <Image rounded fluid src={coffeeBeanImg} />
+      <Image rounded fluid src={coffeeBeanMugImg} />
     </Container>
   );
 };
